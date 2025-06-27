2f34f27c7919bb5446ab9af8bd349f1e7ddabe97
unknown <you@example.com>
2025-06-27 10:49:13 +0400

    finish test script

---
+++ presidio_en.py
@@ -0,0 +1,21 @@
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+import sqlite3
+
+analyzer = AnalyzerEngine()
+anonymizer = AnonymizerEngine()
+
+with sqlite3.connect('foreign_test.db') as db:
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM sentences;')
+
+    print('Analyzing...')
+
+    for data in cursor:
+        results = analyzer.analyze(text=data[1], language='es')
+        anonymized = anonymizer.anonymize(text=data[1], analyzer_results=results)
+        
+        with open('english_result.txt', "a+", encoding="utf8") as f:
+            f.write(f'{anonymized.text}\n\n')
+
+    print('End!')
---
+++ presidio_es.py
@@ -0,0 +1,38 @@
+from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer.predefined_recognizers import EmailRecognizer, PhoneRecognizer
+from presidio_analyzer.nlp_engine import NlpEngineProvider
+from presidio_anonymizer import AnonymizerEngine
+import sqlite3
+
+config = {
+    "nlp_engine_name": "spacy",
+    "models": [{ "lang_code": "es", "model_name": "es_core_news_sm" }]
+}
+
+provider = NlpEngineProvider(nlp_configuration=config)
+nlp_engine = provider.create_engine()
+
+analyzer = AnalyzerEngine()
+email_recognizer = EmailRecognizer(supported_language="es")
+phone_recognizer = PhoneRecognizer(supported_language="es")
+
+registry = analyzer.registry
+registry.add_recognizer(email_recognizer)
+registry.add_recognizer(phone_recognizer)
+
+anonymizer = AnonymizerEngine()
+
+with sqlite3.connect('foreign_test.db') as db:
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM sentences;')
+
+    print('Analyzing...')
+
+    for data in cursor:
+        results = analyzer.analyze(text=data[1], language='es')
+        anonymized = anonymizer.anonymize(text=data[1], analyzer_results=results)
+        
+        with open('spanish_result.txt', "a+", encoding="utf8") as f:
+            f.write(f'{anonymized.text}\n\n')
+
+    print('End!')
